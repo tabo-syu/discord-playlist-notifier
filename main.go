fbf506ceea158635cd20f85bfca2ce02cf63f842
tabo-syu <takeshima.nitkc@gmail.com>
2022-07-09 11:33:35 +0000

    [add] connect db

--- main.go
+++ main.go
@@ -10,25 +10,51 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"google.golang.org/api/option"
 	"google.golang.org/api/youtube/v3"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-// Bot parameters
 var (
+	DB_HOST     = os.Getenv("DB_HOST")
+	DB_PORT     = os.Getenv("DB_PORT")
+	DB_NAME     = os.Getenv("DB_NAME")
+	DB_USER     = os.Getenv("DB_USER")
+	DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	DB_TIMEZONE = os.Getenv("DB_TIMEZONE")
+
 	GUILD_ID      = os.Getenv("GUILD_ID")
 	DISCORD_TOKEN = os.Getenv("DISCORD_ACCESS_TOKEN")
 	YOUTUBE_TOKEN = os.Getenv("YOUTUBE_APIKEY")
 )
 
-var ctx context.Context
-var dc *discordgo.Session
-var yt *youtube.Service
+var (
+	ctx context.Context
+	db  *gorm.DB
+	dc  *discordgo.Session
+	yt  *youtube.Service
+)
 
 func init() {
 	var err error
+
+	dsn := strings.Join([]string{
+		"host=" + DB_HOST,
+		"user=" + DB_USER,
+		"password=" + DB_PASSWORD,
+		"dbname=" + DB_NAME,
+		"port=" + DB_PORT,
+		"sslmode=disable",
+		"TimeZone=" + DB_TIMEZONE,
+	}, " ")
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Could not connect the db: %v", err)
+	}
 
 	dc, err = discordgo.New("Bot " + DISCORD_TOKEN)
 	if err != nil {
