ab61151306a334486e8952dcb87d2f9ad1223426
tabo-syu <takeshima.nitkc@gmail.com>
2022-08-11 14:41:25 +0000

    [fix] check nil

--- repository/youtube_repository.go
+++ repository/youtube_repository.go
@@ -64,6 +64,10 @@ func (r *youTubeRepository) FindPlaylistsWithVideos(ids ...string) ([]*domain.Pl
 		if err != nil {
 			return nil, err
 		}
+		// 動画の登録されていないプレイリストはスキップ
+		if len(playlistItems.Items) == 0 {
+			continue
+		}
 
 		var vids []string
 		for _, item := range playlistItems.Items {
@@ -73,6 +77,10 @@ func (r *youTubeRepository) FindPlaylistsWithVideos(ids ...string) ([]*domain.Pl
 		if err != nil {
 			return nil, err
 		}
+		// プレイリストに登録されているがすべての動画が削除されている場合はスキップ
+		if len(videos.Items) == 0 {
+			continue
+		}
 
 		var cids []string
 		for _, item := range videos.Items {
@@ -81,6 +89,10 @@ func (r *youTubeRepository) FindPlaylistsWithVideos(ids ...string) ([]*domain.Pl
 		channels, err := r.youtube.Channels.List([]string{"id", "snippet"}).MaxResults(MAX_RESULTS).Id(cids...).Do()
 		if err != nil {
 			return nil, err
+		}
+		// プレイリストに登録されているがすべてのチャンネルが削除されている場合はスキップ
+		if len(channels.Items) == 0 {
+			continue
 		}
 
 		var listVideos = []domain.Video{}
@@ -93,6 +105,11 @@ func (r *youTubeRepository) FindPlaylistsWithVideos(ids ...string) ([]*domain.Pl
 					break
 				}
 			}
+			// 動画が削除されている場合は次の動画へ
+			if video == nil {
+				continue
+			}
+
 			var channel *youtube.Channel
 			for _, c := range channels.Items {
 				if c.Id == video.Snippet.ChannelId {
@@ -108,7 +125,7 @@ func (r *youTubeRepository) FindPlaylistsWithVideos(ids ...string) ([]*domain.Pl
 				YoutubeID:        listVideo.Snippet.ResourceId.VideoId,
 				Title:            listVideo.Snippet.Title,
 				Views:            video.Statistics.ViewCount,
-				Thumbnail:        listVideo.Snippet.Thumbnails.High.Url,
+				Thumbnail:        video.Snippet.Thumbnails.High.Url,
 				ChannelName:      channel.Snippet.Title,
 				ChannelIcon:      channel.Snippet.Thumbnails.Default.Url,
 				PublishedAt:      publishedAt,
